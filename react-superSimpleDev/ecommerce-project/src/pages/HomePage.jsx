0a7f86e7a7ed34c96e5478a3fb14152aef68ca97
gobardhanm <gobardhan.meh3r@gmail.com>
2025-10-19 12:26:43 +0530

    fetch data from backend

--- react-superSimpleDev/ecommerce-project/src/pages/HomePage.jsx
+++ react-superSimpleDev/ecommerce-project/src/pages/HomePage.jsx
@@ -3,6 +3,13 @@ import "./HomePage.css";
 import { products } from "../../starting-code/data/products";
 
 function HomePage() {
+  fetch("http://localhost:3000/api/products/")
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+    });
   return (
     <>
       <title>E-Commerce Project</title>
