00fb4f515c095eba93c4b149dd4681e421cef858
gobardhanm <gobardhan.meh3r@gmail.com>
2025-10-20 16:53:48 +0530

    get Server-Proxy config for '/apt' and '/images'

--- react-superSimpleDev/ecommerce-project/src/pages/HomePage.jsx
+++ react-superSimpleDev/ecommerce-project/src/pages/HomePage.jsx
@@ -7,12 +7,14 @@ function HomePage() {
   const [products, setProducts] = useState([]);
   const [cart, setCart] = useState([]);
   useEffect(() => {
-    axios.get("http://localhost:3000/api/products/").then((response) => {
+    axios.get("/api/products/").then((response) => {
       setProducts(response.data);
     });
   }, []);
 
-  axios.get("http://localhost:3000/api/cart-items/").then((response) => {
+  //Server-Proxy configs
+
+  axios.get("/api/cart-items/").then((response) => {
     setCart(response.data);
   });
 
