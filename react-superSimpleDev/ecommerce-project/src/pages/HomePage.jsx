8d8087b1d4561bb25903a5a3ade63a0decb5c959
gobardhanm <gobardhan.meh3r@gmail.com>
2025-10-20 20:41:43 +0530

    generate HTML for the payment summary

--- react-superSimpleDev/ecommerce-project/src/pages/HomePage.jsx
+++ react-superSimpleDev/ecommerce-project/src/pages/HomePage.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import Header from "../components/Header";
 import "./HomePage.css";
-import { fomatMoney } from "../utils/money";
+import { formatMoney } from "../utils/money";
 
 function HomePage({ cart }) {
   const [products, setProducts] = useState([]);
@@ -46,7 +46,7 @@ function HomePage({ cart }) {
                 </div>
 
                 <div className="product-price">
-                  ${fomatMoney(product.priceCents)}
+                  ${formatMoney(product.priceCents)}
                 </div>
 
                 <div className="product-quantity-container">
