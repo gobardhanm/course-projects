107773706e9c27fd8640fddbfd16b2de34497a16
gobardhanm <gobardhan.meh3r@gmail.com>
2025-11-04 16:20:42 +0530

    add new files

---
+++ tailwind-pedroTech/tailwind.config.cjs
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  experimental: {
+    // Allow experimental at-rules if needed by future versions
+  },
+};
